a7b395c75dc0b005525981a3ec84c6281a5f9f13
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-06-28 11:29:20 +0300

    Update eslint errors

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-
-
-let books =[];
-const book_list = JSON.parse(localStorage.getItem('books'));
-if (book_list != null){
-book_list.forEach(element => {
-    books.push(element);
-});
-display();
-}
-// Add book to book list
-function addBook (){
-    let bookTitle = document.getElementById('title').value;
-    let bookAuthor = document.getElementById('author').value;
-    books.push(  {
-        title : bookTitle,
-        author : bookAuthor
-    }); 
-    const booksHolderList = document.getElementById('book_list').innerHTML= '';
-    display();
-    localStorage.setItem('books', JSON.stringify(books));
-}
-
-const add = document.getElementById('add_btn');
-add.addEventListener('click', addBook);
-
-function display () {
-const booksHolderList = document.getElementById('book_list');
-    for (let i = 0; i < books.length; i++) {
-        const item = document.createElement('li');
-        item.innerHTML = `
+const books = [];
+const bookList = JSON.parse(localStorage.getItem('books'));
+// display
+function display() {
+  const booksHolderList = document.getElementById('book_list');
+  for (let i = 0; i < books.length; i += 1) {
+    const item = document.createElement('li');
+    item.innerHTML = `
                     <div>
                         <div>
                             <h2 id="book-title">${books[i].title}</h2>
@@ -38,29 +15,41 @@ const booksHolderList = document.getElementById('book_list');
                             <button class="remove-btn" data-set="${i}">Remove</button>
                         </div>
                     </div>`;
-        booksHolderList.appendChild(item);
-    }
-    removeBook();
-}
+    booksHolderList.appendChild(item);
+  }
 
-function removeBook(){
-
-    const removeBtn = document.querySelectorAll('.remove-btn');
-    removeBtn.forEach(element => {
+  const removeBtn = document.querySelectorAll('.remove-btn');
+  removeBtn.forEach((element) => {
     element.addEventListener('click', () => {
-        books.splice(element.dataset.id ,1);
-        const booksHolderList = document.getElementById('book_list').innerHTML= '';
-        display();
-        localStorage.setItem('books', JSON.stringify(books));
+      books.splice(element.dataset.id, 1);
+      const booksHolderList = document.getElementById('book_list');
+      booksHolderList.innerHTML = '';
+      display();
+      localStorage.setItem('books', JSON.stringify(books));
     });
-    });
+  });
 }
 
+if (bookList != null) {
+  bookList.forEach((element) => {
+    books.push(element);
+  });
+  display();
+}
 
+// Add book to book list
+function addBook() {
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+  books.push({
+    title: bookTitle,
+    author: bookAuthor,
+  });
+  const booksHolderList = document.getElementById('book_list');
+  booksHolderList.innerHTML = '';
+  display();
+  localStorage.setItem('books', JSON.stringify(books));
+}
 
-
-// addBook('Apple', 'Berkay' );
-// addBook('orange', 'Berkay' );
-// addBook('Apple', 'alan' );
-// addBook('orange', 'alan' );
-
+const add = document.getElementById('add_btn');
+add.addEventListener('click', addBook);
